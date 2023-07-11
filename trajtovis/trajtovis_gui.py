7a699e089480ef6de6e46cc1b91e48d305082d82
mirko <mirkoweber95@gmail.com>
2023-07-11 10:16:06 +0200

    visualisation with individual selection of core region, cy3, cy5 and backbone works now

--- trajtovis/trajtovis_gui.py
+++ trajtovis/trajtovis_gui.py
@@ -30,25 +30,29 @@ def run_plugin_gui():
 class App(QtWidgets.QWidget):
     def __init__(self, _pymol_running=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.colorful_residues: list = []
+        self.visualisation_core: list = []
         self.pdbText = None
         self.file_name_pdb: str = ''
         module_dir: pathlib.Path = pathlib.Path(__file__).parent
-        print(module_dir)
-        trajtovis_ui = str(module_dir.joinpath('trajtovis.ui'))
-        print(trajtovis_ui)
-        pdb_show_ui = str(module_dir.joinpath('pdb_show.ui'))
+        trajtovis_ui: str = str(module_dir.joinpath('trajtovis.ui'))
+        pdb_show_ui: str = str(module_dir.joinpath('pdb_show.ui'))
         utils.loadUi(trajtovis_ui, self)
         self.textWindow = QtWidgets.QDialog(self)
         self.file_name_path_pdb = None
         utils.loadUi(pdb_show_ui, self.textWindow)
 
+        # initialize form element states
+        self.show_pdb.setEnabled(False)
+        self.visualisation.setEnabled(False)
+
         # add gui elements logic
         self.load_pdb.clicked.connect(self.load_pdb_button)
-        self.show_pdb.clicked.connect(self.open_pdb_file)
+        self.show_pdb.clicked.connect(self.show_pdb_file)
+        self.visualisation.clicked.connect(self.visualise)
+        self.core_visualisation_selection.clicked.connect(self.add_core_visualisation_selection)
 
 
-    def load_pdb_button(self, file_name_path_pdb=False):
+    def load_pdb_button(self):
         """
         Load PDB or CIF file
 
@@ -65,15 +69,17 @@ class App(QtWidgets.QWidget):
         with open(self.file_name_path_pdb, "r") as f:
             self.pdbText = f.read()
             self.show_pdb.setEnabled(True)
+            self.visualisation.setEnabled(True)
         self.line_current_pdb.setText(self.file_name_pdb)
 
-    def open_pdb_file(self):
+    def show_pdb_file(self):
         """
         Show the PDB file as a text file
         """
-        self.textWindow.textBrowser_pdbFile.setText(self.pdbText)
-        self.textWindow.setWindowTitle(f'{self.file_name_pdb}')
-        is_ok = self.textWindow.exec_()
+        if self.show_pdb.isEnabled():
+            self.textWindow.textBrowser_pdbFile.setText(self.pdbText)
+            self.textWindow.setWindowTitle(f'{self.file_name_pdb}')
+            _ = self.textWindow.exec_()
 
     def add_resi(self):
         current = []
@@ -81,12 +87,56 @@ class App(QtWidgets.QWidget):
         end = self.end_resi.value()
         current.append(start)
         current.append(end)
-        self.colorful_residues.append(current)
+        self.visualisation_core.append(current)
 
-    def visualize(self):
-        cmd.load("/usr/local/lib/python3.8/dist-packages/trajtovis/KLTL_res_traj.pdb")
-        self.structure_coloring()
-        #self.fancy_coloring()
+    def visualise(self):
+        if self.check_backbone.isChecked() or \
+                self.visualisation_core or \
+                self.check_cy3.isChecked() or \
+                self.check_cy5.isChecked():
+            self.visualisation_parameters()
+        if self.visualisation_core:
+            resi_selection = 'resi'
+            for pair in self.visualisation_core:
+                resi_selection = f'{resi_selection} {pair[0]}-{pair[1]}+'
+            resi_selection = resi_selection[:-1]
+            cmd.select("contact", resi_selection)
+            cmd.set("cartoon_ladder_color", "hotpink", "contact")
+            cmd.set("cartoon_ring_mode", "3", "contact")
+            cmd.color("hotpink", "contact")
+            self.visualisation_core.clear()
+        if self.check_backbone.isChecked():
+            cmd.set("cartoon_nucleic_acid_color", "gray")
+            cmd.set("cartoon_ladder_color", "my_blue")
+        if self.check_cy3.isChecked() or self.check_cy5.isChecked():
+            cmd.select("dye_bases", "resn RUM+RGO")
+            cmd.set("cartoon_ring_mode", "3", "dye_bases")
+            cmd.show("sticks", "dye_bases")
+            cmd.hide("sticks",
+                     "dye_bases and name O4+H3+O2+H6+H1'+H2'1+HO'2+O2'+H4'+N3+C2+C4+C6+H6+N1+C1'+O4'+C3'+C4'+C5'+H5'+H5'2+O5'+O1P+O2P+P+H22+H8+H21+N2+N7+N9+H73")
+            if self.check_cy3.isChecked():
+                cmd.select("d_cy3", "resn RGO+C3W")
+                cmd.color("don_cy3_green", "d_cy3")
+                cmd.set("cartoon_ladder_color", "don_cy3_green", "d_cy3")
+            if self.check_cy5.isChecked():
+                cmd.select("d_cy5", "resn RUM+C5W")
+                cmd.color("acc_cy5_red", "d_cy5")
+                cmd.set("cartoon_ladder_color", "acc_cy5_red", "d_cy5")
+            cmd.set("cartoon_ring_transparency", "0.7")
+
+
+    @staticmethod
+    def visualisation_parameters():
+        cmd.bg_color("white")
+        cmd.set("ray_trace_mode", "3")
+        cmd.set_color("my_blue", "[103,169,207]")
+        cmd.set_color("don_cy3_green", "[108,189,129]")
+        cmd.set_color("acc_cy5_red", "[194,84,73]")
+
+    def add_core_visualisation_selection(self):
+        start = self.core_visualisation_start.value()
+        end = self.core_visualisation_end.value()
+        self.visualisation_core.append((start, end))
 
     def fancy_coloring(self):
         cmd.show("ribbon")
@@ -109,40 +159,3 @@ class App(QtWidgets.QWidget):
     def aligner(self, name, core, model_range):
         for i in np.arange(model_range[0], model_range[1]):
             cmd.align(f"{name}{'{:04d}'.format(i)}", core)
-
-    def structure_coloring(self):
-        cmd.bg_color("white")
-        cmd.set("cartoon_nucleic_acid_color", "gray")
-        cmd.set("ray_trace_mode", "3")
-        cmd.set_color("my_blue", "[103,169,207]")
-        cmd.set("cartoon_ladder_color", "my_blue")
-        cmd.set_color("don_green", "[108,189,129]")
-        cmd.set_color("acc_red", "[194,84,73]")
-
-        cmd.select("dye_bases", "resn RUM+RGO")
-        cmd.set("cartoon_ring_mode", "3", "dye_bases")
-        cmd.show("sticks", "dye_bases")
-        cmd.hide("sticks",
-                 "dye_bases and name O4+H3+O2+H6+H1'+H2'1+HO'2+O2'+H4'+N3+C2+C4+C6+H6+N1+C1'+O4'+C3'+C4'+C5'+H5'+H5'2+O5'+O1P+O2P+P+H22+H8+H21+N2+N7+N9+H73")
-
-        if len(self.colorful_residues) != 0:
-            hpresi = "resi "
-            for group in self.colorful_residues:
-                hpresi = hpresi + str(group[0]) + "-" + str(group[1]) + "+"
-            hpresi = hpresi[:-1]
-            print(hpresi)
-            cmd.select("contact", hpresi)
-            # cmd.color("hotpink", "contact")
-            cmd.set("cartoon_ladder_color", "hotpink", "contact")
-            cmd.set("cartoon_ring_mode", "3", "contact")
-            cmd.color("hotpink", "contact")
-
-        cmd.select("d_cy5", "resn RUM+C5W")
-        cmd.select("d_cy3", "resn RGO+C3W")
-        cmd.color("acc_red", "d_cy5")
-        cmd.color("don_green", "d_cy3")
-        cmd.set("cartoon_ladder_color", "acc_red", "d_cy5")
-        cmd.set("cartoon_ladder_color", "don_green", "d_cy3")
-        cmd.set("cartoon_ring_transparency", "0.7")
-
-        self.colorful_residues.clear()
